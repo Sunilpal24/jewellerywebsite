dac41a5504a666dcc686189522349fe4a93800e8
Sunilpal24 <116942745+Sunilpal24@users.noreply.github.com>
2023-01-17 11:45:48 +0530

    Update Page404.js

--- src/Components/Page404.js
+++ src/Components/Page404.js
@@ -7,7 +7,7 @@ const Page404 = () => {
     <div className='text-center error'>
       <h1>404</h1>
       <h1>Page Not Found</h1>
-      <p>We're sorry,the page you requestcould not be found</p>
+      <p>We're sorry,the page you request could not be found</p>
       <p>Please go back to the homepage</p>
       <Link to="/"><button className='btn btn-outline-primary rounded-pill px-4'>Go Home</button></Link>
     </div>
